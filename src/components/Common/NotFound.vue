693151b47869fc4b8eee9113360147b81bd1cc62
andre1ka-ber <appinsider@outlook.com>
2022-06-13 11:43:10 +0500

    added flex-grow property to not-found-page

--- src/components/Common/NotFound.vue
+++ src/components/Common/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex align-items-center text-center flex-column section-padding-100">
+  <div class="not-found-page d-flex align-items-center text-center flex-column section-padding-100">
     <img src="@/assets/images/core-img/404.svg" class="error__image" />
     <h3 class="text-danger mt-4">
       <slot>Ошибка 404. Page not found!</slot>
@@ -19,8 +19,12 @@ export default {
 </script>
 
 <style scoped>
+.not-found-page {
+  flex-grow: 1;
+}
 .error__image {
   width: 30%;
+  max-height: 420px;
   position: relative;
 }
 a {
